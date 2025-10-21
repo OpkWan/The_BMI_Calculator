7a3782f8e9c0a6f6abb220a947f11695e25ccc3e
Najeeb Dollah <marsfelloff@gmail.com>
2025-10-21 19:06:38 +0000

    Creating the interface

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,3 +2,9 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk, messagebox
 from PIL import Image,ImageTk
+
+root=Tk()
+root.title("BMI Calculator")
+root.geometry("470x580+300+200")
+root.resizable(False,False)
+root.configure(bg="#f0f1f5")
