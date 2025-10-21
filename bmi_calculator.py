0f23ce2dcc5ce859b30805ff66416831fc1cb9e7
Najeeb Dollah <marsfelloff@gmail.com>
2025-10-21 19:16:55 +0000

    Adding an icon

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -8,3 +8,10 @@ root.title("BMI Calculator")
 root.geometry("470x580+300+200")
 root.resizable(False,False)
 root.configure(bg="#f0f1f5")
+
+try:
+    image_icon=PhotoImage(file="Images/icon.png")
+    root.iconphoto(False,image_icon)
+except:
+    pass
+
